7952969e068bf0c51dd489f7a91b835ea1856197
William Morrison <williamchrismorrison@gmail.com>
2020-06-29 08:54:49 -0700

    completed exercise

--- CodingEventsDemo/Models/Event.cs
+++ CodingEventsDemo/Models/Event.cs
@@ -11,14 +11,20 @@ namespace CodingEventsDemo.Models
 
         public string ContactEmail { get; set; }
 
+        public string Location { get; set; }
+
+        public string AttendanceCount { get; set; }
+
         public int Id { get; }
         static private int nextId = 1;
 
-        public Event(string name, string description, string contactEmail)
+        public Event(string name, string description, string contactEmail, string location, string attendanceCount)
         {
             Name = name;
             Description = description;
             ContactEmail = contactEmail;
+            Location = location;
+            AttendanceCount = attendanceCount;
             Id = nextId;
             nextId++;
         }
--- CodingEventsDemo/ViewModels/AddEventViewModel.cs
+++ CodingEventsDemo/ViewModels/AddEventViewModel.cs
@@ -13,6 +13,13 @@ namespace CodingEventsDemo.ViewModels
         [StringLength(500, ErrorMessage = "Description too long!")]
         public string Description { get; set; }
 
+        [Required(ErrorMessage = "Location is required")]
+        public string Location { get; set; }
+
+        [Required(ErrorMessage = "Attendance count is required.")]
+        [Range(0, 100000, ErrorMessage = "Attendance count must be between 0 and 100,000 people")]
+        public string AttendanceCount { get; set; }
+
         [EmailAddress]
         public string ContactEmail { get; set; }
     }
